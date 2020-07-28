f650615fcdcfe772e94e326b619915d028513c06
Anca Simon <ancarsimon@gmail.com>
2020-07-28 02:10:53 -0500

    Got the single letters to display the appropriate number of times.

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.Serialization.Formatters;
 
 namespace LetterLoops
@@ -11,23 +12,28 @@ namespace LetterLoops
 
             //1 - capture the word from the user:
             var userInput = Console.ReadLine();
-            Console.WriteLine($"userInput: { userInput }");
+            //Console.WriteLine($"userInput: { userInput }");
 
             //2 - change the word into an array of single characters;
             char[] charArray = userInput.ToCharArray();
-            Console.WriteLine($"charArray: { charArray }");
+            //Console.WriteLine($"charArray: { charArray }");
 
-            var finalCharList = charArray;
+            //ALSO need to create a copy of the char array so that I can push the final result of each letter that I want to print into the appropiate index places in this array:
+            var finalCharList = charArray.Select( c => c.ToString()).ToArray();
+            //Console.WriteLine($"finalCharList when first copied: { finalCharList }");
 
-            //3 - loop over each character in the new array and print it out (just once at this point!):
+            //3 - loop over each character in the new char array and print it out (just once at this point!) and then push each character into the new finalCharList I created as a copy above:
             for (int i = 0; i < charArray.Length; i++)
             {
-                Console.WriteLine(charArray[i]);
-                var singleChar = charArray[i];
+                //Console.WriteLine(charArray[i]);
+                var indexNum = i;
+                var singleChar = string.Join("", Enumerable.Repeat(charArray[i], i+1));
+                //Console.WriteLine(i);
                 finalCharList[i] = singleChar;
             }
 
             Console.WriteLine($"Your word is made up of:" + string.Join('-', finalCharList));
         }
+
     }
 }
